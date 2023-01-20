b6bc657d1483c6f607ca01732c345d410ba6ca4a
stonem2 <stonem2@muhs.edu>
2023-01-19 20:57:58 -0600

    First swerve offsets created

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -28,12 +28,12 @@ public final class Constants {
 
   // FIXME: an empty string uses the default CAN bus; specify the name of the CANivore as
   // appropriate
-  public static final String CAN_BUS_NAME = "po";
+  public static final String CAN_BUS_NAME = "";
 
   // FIXME: specify the name of the camera used for detecting AprilTags
   public static final String CAMERA_NAME = "ov9268";
 
-  private static final RobotType ROBOT = RobotType.ROBOT_SIMBOT;
+  private static final RobotType ROBOT = RobotType.ROBOT_2023_PRESEASON;
 
   private static final Alert invalidRobotAlert =
       new Alert("Invalid robot selected, using competition robot as default.", AlertType.ERROR);
